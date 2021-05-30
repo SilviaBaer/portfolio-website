bf344b4602939b2265f8494c87f59ceafd726ae5
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-05-30 23:48:31 +0200

    ✨ import Greeting component

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Greeting from "./components/greeting/greeting"
 
 function App() {
   return (
     <div className="App">
     <h1>Hello!</h1>
+    <Greeting/>
     </div>
   );
 }
