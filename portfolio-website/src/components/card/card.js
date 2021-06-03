69e0acdbca9367d7fa07ca721ed74e47ea2baa85
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-03 22:39:59 +0200

    ✨ add card component (dummy

--- portfolio-website/src/components/card/card.js
+++ portfolio-website/src/components/card/card.js
@@ -2,7 +2,12 @@ import styles from "../card/card.module.css"
 
 function Card() {
     return (
-        <h1 className={styles.card}>Hi!!</h1>
+<div className={styles.col}>
+            <a href="/"/>
+            <img src="" alt="project-1"/>
+            <h3 className={styles.title}>Hi!!</h3>
+            <p className={styles.what}>Lorem Ipsum lorem</p>
+        </div>
     )
 }
 
