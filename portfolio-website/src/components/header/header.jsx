30aecb545611d35b417118616333bc25fd209c8d
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-03 19:52:42 +0200

    ✨ add header component

---
+++ portfolio-website/src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import styles from "../header/header.module.css"
+
+function Header() {
+    return (
+        <div className={styles.header}>
+<h3><a href="https://react-bootstrap.github.io/components/navs/">Hi!!</a></h3>
+<h3><a href="https://react-bootstrap.github.io/components/navs/">Hi!!</a></h3>
+<h3><a href="https://react-bootstrap.github.io/components/navs/">Hi!!</a></h3>
+
+        </div>
+        
+    )
+}
+
+export default Header
