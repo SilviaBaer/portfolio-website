0c731d18faa95363246cac03b894e2a1d453d25b
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-03 18:20:30 +0200

    ✨ import pro component

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Name from "./components/name/name"
+import Pro from "./components/pro/pro"
 
 function App() {
   return (
     <div className="App">
     <Name/>
+    <Pro/>
     </div>
   );
 }
