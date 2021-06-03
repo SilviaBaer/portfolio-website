b1b58ce05d8422f9c790091e8a6a2bd808ff6c56
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-03 21:55:20 +0200

    🐛 update links

--- portfolio-website/src/components/header/header.jsx
+++ portfolio-website/src/components/header/header.jsx
@@ -3,9 +3,9 @@ import styles from "../header/header.module.css"
 function Header() {
     return (
         <div className={styles.header}>
-<h3><a href="https://react-bootstrap.github.io/components/navs/">Hi!!</a></h3>
-<h3><a href="https://react-bootstrap.github.io/components/navs/">Hi!!</a></h3>
-<h3><a href="https://react-bootstrap.github.io/components/navs/">Hi!!</a></h3>
+<h3><a href="/">Hi!!</a></h3>
+<h3><a href="/">Hi!!</a></h3>
+<h3><a href="/">Hi!!</a></h3>
 
         </div>
         
