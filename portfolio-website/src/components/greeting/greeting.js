e5928f8d903774a194e150d04544000ce003d290
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-03 15:01:03 +0200

    ✨ add greeting component

--- portfolio-website/src/components/greeting/greeting.js
+++ portfolio-website/src/components/greeting/greeting.js
@@ -1,9 +1,24 @@
 import styles from "../greeting/greeting.module.css"
 
 function Greeting() {
+    const date = new Date();
+    const currentTime = date.getHours();
+  
+    let greeting;
+  
+    if (currentTime < 12) {
+      greeting = "Good Morning!!";
+    } else if (currentTime < 18) {
+      greeting = "Good Afternoon!!";
+    } else {
+      greeting = "Good Night!!";
+    }
+  
     return (
-        <h1 className={styles.hello}>Hi!!</h1>
-    )
-}
-
-export default Greeting
+      <h1 className={styles.greeting}>
+        {greeting}
+      </h1>
+    );
+  }
+  
+  export default Greeting;
