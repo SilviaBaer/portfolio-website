c293a6a137e8899d791d0e1c241c31d722be6f0f
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-03 18:19:43 +0200

    ✨ add pro component

---
+++ portfolio-website/src/components/pro/pro.jsx
@@ -0,0 +1,9 @@
+import styles from "../pro/pro.module.css"
+
+function Pro() {
+    return (
+        <h2 className={styles.pro}>A <span className={styles.span}>pro</span>grammer in <span className={styles.span}>pro</span>gress</h2>
+    )
+}
+
+export default Pro
