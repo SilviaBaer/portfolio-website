8715bd03f70343489f4d30ae38117f0002f141ae
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-03 22:40:30 +0200

    ✨ add cards container component

---
+++ portfolio-website/src/components/card/cards.js
@@ -0,0 +1,15 @@
+import styles from "../card/card.module.css"
+import Card from "../card/card"
+
+function Cards() {
+    return (
+        <div className={styles.cardsContainer}>
+            <Card/>
+            <Card/>
+            <Card/>
+
+        </div>
+    )
+}
+
+export default  Cards;
