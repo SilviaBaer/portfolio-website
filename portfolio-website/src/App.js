2aed504148b2ec05df5d4bddf1d4ea2b8dc87f3a
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-03 18:50:46 +0200

    📝 add link to styles Bootstrap

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Name from "./components/name/name"
 import Pro from "./components/pro/pro"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
