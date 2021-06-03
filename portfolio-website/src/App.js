90bbc1705b687d01bf97eaa19c25094fa8a09b2d
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-03 22:41:31 +0200

    ✨ import cards container component

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,16 +1,19 @@
-import './App.css';
+import styles from './App.css';
 import Name from "./components/name/name"
 import Pro from "./components/pro/pro"
 import Header from "./components/header/header"
 import Footer from "./components/footer/footer"
+import Cards from "./components/card/cards"
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+    
     <Header/>
     <Name/>
     <Pro/>
+    <Cards/>
     <Footer/>
     </div>
   );
