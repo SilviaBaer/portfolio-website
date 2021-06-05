d9e7a79b50362e0dbe5890b83f2fdef2980ebae1
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-05 18:11:02 +0200

    ✨ add links to pages in header

--- portfolio-website/src/components/header/header.jsx
+++ portfolio-website/src/components/header/header.jsx
@@ -3,8 +3,8 @@ import styles from "../header/header.module.css"
 function Header() {
     return (
         <div className={styles.header}>
-<h3><a href="/">Hi!!</a></h3>
-<h3><a href="/">Hi!!</a></h3>
+<h3><a href="/about">About</a></h3>
+<h3><a href="/projects">Projects</a></h3>
 <h3><a href="/">Hi!!</a></h3>
 
         </div>
