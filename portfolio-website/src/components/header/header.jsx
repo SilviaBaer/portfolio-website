e509739be44e172b4d1bc5004e388bedfaea7dae
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-05 22:46:34 +0200

    ✨ add link to welcome page in header

--- portfolio-website/src/components/header/header.jsx
+++ portfolio-website/src/components/header/header.jsx
@@ -1,13 +1,12 @@
 import styles from "../header/header.module.css"
 import { NavLink } from "react-router-dom"
-import classes from "../header/header.module.css"
 
 function Header() {
     return (
         <div className={styles.header}>
 <h3><NavLink activeClassName={styles.active} to="/about">About</NavLink></h3>
 <h3><NavLink activeClassName={styles.active} to="/projects">Projects</NavLink></h3>
-<h3><NavLink activeClassName={styles.active} to="/">Home</NavLink></h3>
+<h3><NavLink activeClassName={styles.active} to="/welcome">Home</NavLink></h3>
 
         </div>
         
