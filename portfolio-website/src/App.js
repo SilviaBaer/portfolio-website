3115f1c35177e2902996ab9571f0b325b5ca8438
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-05 18:48:18 +0200

    ⚡️ add dynamic segment to project route

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Route path="/projects">
         <Projects/>
       </Route>
-      <Route path="/project">
+      <Route path="/project/:id">
         <Projects/>
       </Route>
       <Name/>
