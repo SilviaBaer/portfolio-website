4098105f76736b541a0c0937c517e8713c1ff88f
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-05 18:18:59 +0200

    ⚡️ replace a with Link component

--- portfolio-website/src/components/header/header.jsx
+++ portfolio-website/src/components/header/header.jsx
@@ -1,11 +1,12 @@
 import styles from "../header/header.module.css"
+import { Link } from "react-router-dom"
 
 function Header() {
     return (
         <div className={styles.header}>
-<h3><a href="/about">About</a></h3>
-<h3><a href="/projects">Projects</a></h3>
-<h3><a href="/">Hi!!</a></h3>
+<h3><Link to="/about">About</Link></h3>
+<h3><Link to="/projects">Projects</Link></h3>
+<h3><Link to="/">Home</Link></h3>
 
         </div>
         
