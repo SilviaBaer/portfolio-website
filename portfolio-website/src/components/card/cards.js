61766c698fddea0292f278e0bd86732ed4a14506
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-06 19:21:56 +0200

    ✨ map through array of objects

--- portfolio-website/src/components/card/cards.js
+++ portfolio-website/src/components/card/cards.js
@@ -1,18 +1,24 @@
 import styles from "../card/card.module.css"
 import Card from "../card/card"
+import projects from "../../data/projectsList"
+
+function createCard(project) {
+    return <Card
+    key={project.id}
+    img={project.img}
+    title={project.title}
+    what={project.description}
+    more={project.page}
+    />
+}
 
 function Cards() {
+    console.log(projects)
     return (
         <div className={styles.cardsContainer}>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+            {projects.map(createCard)}
+            
+            
 
         </div>
     )
