dbeabb38aa4fa530851bd7e08edf946e448892ba
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-06 17:16:21 +0200

    ✨ dummy code example

--- portfolio-website/src/pages/projects.js
+++ portfolio-website/src/pages/projects.js
@@ -1,13 +1,19 @@
 //a page is just a regular React component, that conventionally is stored in page folder 
 //in order to stress the fact that it is working with router!
 import styles from "../pages/projects.module.css"
-import Cards from "../components/card/cards"
+import Cards from "../components/card/cards"//<Cards/>
+import {Link} from "react-router-dom"
 
 export default function Projects() {
     return (
         <div>
         <h1 className={styles.title}>My Projects</h1>
-        <Cards/>
+            <Cards/>
         </div>
     )
 }
+
+
+{/* <Link to="/projects/p1"><li className={styles.item}>A book</li></Link>
+<Link to="/projects/p2"><li className={styles.item}>An Apple</li></Link>
+<Link to="/projects/p3"><li className={styles.item}>A Cup</li></Link> */}
