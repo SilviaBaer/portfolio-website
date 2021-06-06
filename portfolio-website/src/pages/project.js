2474c44951bd5e7b5251bb7a4c0382e20002e992
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-06 22:30:14 +0200

    ✨ output data on the detailled project page

--- portfolio-website/src/pages/project.js
+++ portfolio-website/src/pages/project.js
@@ -1,14 +1,24 @@
 import styles from "./project.module.css"
 import {useParams} from "react-router-dom"
+import projects from "../data/projectsList"
+import ProjectPage from "../components/projectPage/projectPage"
 
 export default function Detail() {
 const params = useParams();
 console.log(params.id)
 
+const project = projects.find((project) => project.page === params.id)
+if (!project) {
+    return <p className={styles.p}>No Project Found!</p>
+}
+
     return (
         <section>
             <h1 className={styles.title}>Project in Details</h1>
             <p className={styles.p}>{params.id}</p>
+            <div className={styles.projectCard}>
+                <ProjectPage title={project.title} deep={project.deep} />
+            </div>
         </section>
         
     )
