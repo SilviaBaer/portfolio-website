11c3d46023df8560c504d1b0c28c435b524fb0a4
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-06 19:21:21 +0200

    🗃 add projects database

---
+++ portfolio-website/src/data/projectsList.js
@@ -0,0 +1,29 @@
+const projects = [
+    {
+        id: 1,
+        img: "bakend.png", 
+        title:"Bakend",
+        description:"Converter App for Cake Ingredients", 
+        page:"/projects/bakend",
+        deploy: "https://bakend-app.herokuapp.com/"
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1618422689173-3dbcdeb82fa7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmVycmllc3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        title: "Keep Me",
+        description: "Keeper App",
+        page: "/projects/keep-me",
+        deploy: ""
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1576489922094-2cfe89fb1733?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2hlcnJ5JTIwYmxvc3NvbXxlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" ,
+        title: "Newsletter",
+        description: "Form for Newsletter Subscription",
+        page: "/projects/newsletter",
+        deploy:""
+
+    }
+]
+
+export default projects
