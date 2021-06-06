90ee68f7887c6b9aef7419c4b1f37a9f09b21cee
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-06 16:08:39 +0200

    🐛 option 2:  use exact prop

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -17,11 +17,11 @@ function App() {
       <Route path="/about">
         <About/>
       </Route>
+      <Route path="/projects" exact>
+        <Projects/>
+      </Route>
       <Route path="/projects/:id">
         <Project/>
-      </Route>
-      <Route path="/projects">
-        <Projects/>
       </Route>
       <Route path="/welcome">
         <Welcome/>
@@ -44,4 +44,11 @@ export default App;
 //when the server hits the line of code that contains a match
 //switch is going to make it stop immediately, ignoring whatever follows 
 //no matter how specific it is
+
+//OPTION 1. SWAPPING CODE LINES POSITION
 //That is why the position must be swapped!!
+//thze first line is a match and it is going to ignore what follows
+
+//OPTION 2. USING EXACT PROP
+//using the exact prop will make React Router stop when only the EXACT match is found!
+//no need to swap positions
