ff590a999aecfd0c9f64ae678d251a0d5a0ee7fb
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-06 19:19:51 +0200

    ✨ add props to Card component

--- portfolio-website/src/components/card/card.js
+++ portfolio-website/src/components/card/card.js
@@ -1,19 +1,18 @@
 import styles from "../card/card.module.css"
 import {Link} from "react-router-dom"
 
-function Card() {
+function Card(props) {
     return (
-<div className={styles.card}>
-    <div>
-            <img className={styles.img}  src="https://images.unsplash.com/photo-1581798459219-318e76aecc7b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FuZGllc3xlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="project-1"/>
-            
-    </div>
-            <div className={styles.caption}>
-            <h3 className={styles.title}>Hi!!</h3>
-            <p className={styles.what}>Lorem Ipsum lorem</p>
-            <Link className={styles.try} to="/products/p1">Try it!</Link>
-            </div>
+    <div className={styles.card}>
+        <div>
+        <img className={styles.img} src={props.img} alt="project-1"/>
         </div>
+        <div className={styles.caption}>
+        <h3 className={styles.title}>{props.title}</h3>
+        <p className={styles.what}>{props.what}</p>
+        <Link to={props.more}><p className={styles.more}>More!</p></Link>
+        </div>
+    </div>
     )
 }
 
@@ -22,3 +21,6 @@ export default Card
 {/* <Link to="/projects/p1"><li className={styles.item}>A book</li></Link>
 <Link to="/projects/p2"><li className={styles.item}>An Apple</li></Link>
 <Link to="/projects/p3"><li className={styles.item}>A Cup</li></Link> */}
+
+//"/products/p1"
+//""
