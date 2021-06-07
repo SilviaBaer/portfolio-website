002a4a9c714d106c367c96bb4a7e1e8e96105e20
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-07 13:48:10 +0200

    🔥 remove comments

--- portfolio-website/src/components/card/card.js
+++ portfolio-website/src/components/card/card.js
@@ -4,11 +4,9 @@ import {Link} from "react-router-dom"
 function Card(props) {
     return (
     <div className={styles.card}>
-        <div>
-        <img className={styles.img} src={props.img} alt="project-1"/>
-        </div>
         <div className={styles.caption}>
         <h3 className={styles.title}>{props.title}</h3>
+        <img className={styles.img} src={props.img} alt="project-1"/>
         <p className={styles.what}>{props.what}</p>
         <Link to={`/projects/${props.more}`}><p className={styles.more}>More!</p></Link>
         </div>
@@ -17,10 +15,3 @@ function Card(props) {
 }
 
 export default Card
-
-{/* <Link to="/projects/p1"><li className={styles.item}>A book</li></Link>
-<Link to="/projects/p2"><li className={styles.item}>An Apple</li></Link>
-<Link to="/projects/p3"><li className={styles.item}>A Cup</li></Link> */}
-
-//"/products/p1"
-//""
