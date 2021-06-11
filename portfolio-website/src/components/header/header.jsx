bea97eb3568381c92396c23b0fcf60b7f691d445
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-06-12 01:47:00 +0200

    🔥 add comments

--- portfolio-website/src/components/header/header.jsx
+++ portfolio-website/src/components/header/header.jsx
@@ -6,7 +6,7 @@ function Header() {
         <div className={styles.header}>
 <h3 className={styles.linkToPage}><NavLink activeClassName={styles.active} to="/about">About</NavLink></h3>
 <h3 className={styles.linkToPage}><NavLink activeClassName={styles.active} to="/projects">Portfolio</NavLink></h3>
-<h3 className={styles.linkToPage}><NavLink activeClassName={styles.active} to="/contact">Contact</NavLink></h3>
+
 <h3 className={styles.linkToPage}><NavLink activeClassName={styles.active} to="/welcome">Home</NavLink></h3>
 
         </div>
@@ -15,3 +15,6 @@ function Header() {
 }
 
 export default Header
+
+{/* <h3 className={styles.linkToPage}><NavLink activeClassName={styles.active} to="/contact">Contact</NavLink></h3>
+<h3 className={styles.linkToPage}><NavLink activeClassName={styles.active} to="/blog">Blog</NavLink></h3> */}
